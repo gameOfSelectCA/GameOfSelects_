69d74ff9186fbd9957f7badfdf978e3d34f02140
LuizMiguelAdS <luiz.asilva@sptech.school>
2023-06-01 14:33:50 -0300

    o inicio do script

---
+++ worever.sql
@@ -0,0 +1,90 @@
+create database gameSelect
+
+use gameSelect
+
+create table autor(
+idAutor,
+nome,
+)
+
+create table mangaka(
+idMangaka,
+nome,
+)
+
+create table produrora(
+idProdurora,
+nome,
+)
+
+create table destribuidora(
+idDestribuidora,
+nome,
+)
+
+
+create table obra(
+    idObra,
+    nome,
+)
+
+create table lightnovel(
+idLightnovel,
+nome,
+)
+
+create table manga(
+idManga,
+nome,
+)
+
+create table manhawa(
+idManhawa,
+nome,
+)
+
+create table anime(
+idAnime,
+nome,
+)
+
+create table personagem(
+idPersonagem,
+nome,
+alcunha,
+)
+
+create table generoPrincipal(
+idGeneroPrincipal
+nome
+)
+
+create table episodio(
+idEpisodio
+nome
+)
+
+create table capitulo(
+idCapitulo
+nome
+)
+
+create autor(
+idAutor
+nome
+)
+
+create autor(
+idAutor
+nome
+)
+
+create autor(
+idAutor
+nome
+)
+
+create autor(
+idAutor
+nome
+)
