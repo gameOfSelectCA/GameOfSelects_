f64e04fa5de5a87ca568d6d33a7c97bb8d846129
JooaoVSilva <126405719+JooaoVSilva@users.noreply.github.com>
2023-06-01 15:13:31 -0300

    alala

--- worever.sql
+++ worever.sql
@@ -3,30 +3,54 @@ create database gameSelect
 use gameSelect
 
 create table autor(
-idAutor,
-nome,
-)
+idAutor int primary key auto_increment,
+nome varchar(45),
+dtNasc date,
+statusvida varchar(5),
+constraint statusvida check 
+(statusvida in ('vivo' or 'morto'))
+);
 
 create table mangaka(
-idMangaka,
-nome,
-)
+idMangaka int primary key auto_increment,
+nome varchar(45),
+idade int,
+statusvida varchar(5),
+statusvida check 
+(statusvida in ('vivo' or 'morto'))
+)auto_increment = 50;
 
-create table produrora(
-idProdurora,
-nome,
-)
+create table produtora(
+idProdutora int primary key auto_increment,
+nome varchar(45),
+dtCriacao date
+)auto_increment = 100;
 
 create table destribuidora(
-idDestribuidora,
-nome,
-)
+idDestribuidora int primary key auto_increment,
+nome varchar(45),
+dtCriacao date
+)auto_increment = 150;
 
 
 create table obra(
-    idObra,
-    nome,
-)
+idObra int auto_increment,
+nome varchar(45),
+fkAutor int,
+constraint fkAutor foreign key (fkAutor)
+    references autor(idAutor),
+fkGenero int,
+constraint fkGenero foreign key (fkGenero)
+    references generoPrincipal(idGeneroPrincipal),
+fkProtagonistaObra int,
+constraint fkProtagonistaObra foreign key (fkProtagonistaObra)
+    references personagem(fkProtagonista),
+fkPersonagem int,
+constraint fkPersonagem foreign key (fkPersonagem)
+    references personagem(idPersonagem)
+constraint pkObra primary key 
+    (idObra,fkAutor,fkGenero,fkProtagonistaObra,fkPersonagem)
+)auto_increment = 200;
 
 create table lightnovel(
 idLightnovel,
