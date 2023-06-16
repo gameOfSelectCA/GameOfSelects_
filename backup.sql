815e79d5618307c44f977d9bdc65fc8289f4ca65
LuizMiguelAdS <luiz.asilva@sptech.school>
2023-06-16 09:46:24 -0300

    insert Inyasha

---
+++ backup.sql
@@ -0,0 +1,301 @@
+insert into autor values
+(null,'Rumiko Takahashi','1957-10-10','vivo'),
+-- xpto 4
+(null,'Natsume Akatsuki.',null,'vivo'),
+-- xpto 5
+;
+insert into mangaka values
+(null,'Rumiko Takahashi','1957-10-10','vivo'),
+-- xpto 54
+(null,'Kurone Mishima',null,'vivo'),
+-- xpto 55
+(null,'Masahito Watari',null,'vivo')
+-- xpto 56
+;
+
+insert into destribuidora values
+(null,'Weekly Shōnen Sunday','1959-05-17'),-- xpto 156
+(null,'Kadokawa Sneaker Bunko',''), -- xpto 157
+(null,'Kadokawa Shoten','1945-11-10'), --xpto 158
+
+;
+insert into produtora values 
+(null,'Yomiuri TV','1958-02-13'),-- xpto 112
+(null,' Studio Deen ','1975-05-14'); 
+
+
+insert into personagem values
+(null,'Inuyasha',200, null,'conseguir todos os fragmentos das joias de quatro almas.'),
+-- xpto 1003
+(null, 'Kagome',15, 'reencarnação da kykio', 'recuperar os fragmentos da joia de quatro almas.'),
+-- xpto 1004
+(null,'Sesshoumaru',200, null,'se vingar do Naraku'),
+-- xpto 1005
+(null,'Kasuma',17,'viver'),
+-- xpto 1006
+(null,'Megumin',14,'se tornar a maior maga de explosôes'),
+
+;
+insert into obra values
+(null,'Sengoku Otogizōshi InuYasha',4,1506,1003),
+-- xpto 203 
+(null,'Kono Subarashii Sekai ni Shukufuku o!',5,1509,1006)
+
+insert into manga values
+(null,'Sengoku Otogizōshi InuYasha',203,4,156,54),
+-- xpto 2051 
+(null,'Kono Subarashii Sekai ni Shukufuku o!',204,5,158,56);
+-- xpto 2052
+
+insert into lightnovel values
+(null,'Inuyasha',203,4,156,54),-- xpto 2051 
+(null,'Kono Subarashii Sekai ni Shukufuku o!',204,5,157,55);
+-- xpto 2052
+
+
+
+insert into anime values
+(null,'inuyasha',203,4,102) 
+(null,'Kono Subarashii Sekai ni Shukufuku o!',204,5,113);
+
+-- xpto ,2052
+;
+
+insert into saga values
+(null,'inuyasha temporada-1',2052,2051,2051,null,105,105,null),--8
+(null,'inuyasha temporada-2',2052,2051,2051,null,105,105,null),
+(null,'inuyasha temporada-3',2052,2051,2051,null,105,105,null),
+(null,'inuyasha temporada-4',2052,2051,2051,null,105,105,null),
+(null,'inuyasha temporada-5',2052,2051,2051,null,105,105,null),
+(null,'inuyasha temporada-6',2052,2051,2051,null,105,105,null),
+(null,'')
+
+insert into episodio values
+(null,'A Garota que Volta no Tempo e o Garoto Lacrado',8,2052,112),
+(null, 'As pessoas atrás da Jóia de Quatro Almas',8,2052,112),
+(null, 'A volta pelo poço ComeOssos',8,2052,112),
+(null, 'Yura dos Cabelos Invertidos',8,2052,112),
+(null, 'O Temível Príncipe Sesshoumaru',8,2052,112),  
+(null, 'Tessaiga: o canino de aço destruidor',8,2052,112),  
+(null, 'O confronto: Sesshoumaru vs. Tessaiga',8,2052,112),
+(null, 'O Senhor Feudal youkai: Sapo de tsukumo',8,2052,112), 
+(null, 'Entra em cena Shippou!; Os irmãos youkai relâmpago Hitten e Manten',8,2052,112),
+(null, 'Confronto da espada: relâmpago contra Tesaiga',8,2052,112),
+(null, 'A máscara da maldição que ressuscita na época actual',8,2052,112),
+(null, 'Tatarimoke e o pequeno espírito mau',8,2052,112),
+(null, 'O segredo do primeiro dia: Inuyasha de cabelos pretos',8,2052,112),
+(null, 'O roubo dos ossos sagrados da Kikyou',8,2052,112),
+(null, 'O renascimento da infeliz sacerdotisa Kikyou',8,2052,112),
+(null, 'O buraco do vento na mão direita: Miroku, o monge delinquente',8,2052,112),
+(null, 'A tinta corrompida do pintor do Inferno',8,2052,112),
+(null, 'A aliança de Naraku e Sesshoumaru',8,2052,112),
+(null, 'Volte para a sua era, Kagome!',8,2052,112),
+(null, 'O enigma de Onigumo, o ladrão diabólico',8,2052,112),
+(null, 'A verdade sobre a origem de Naraku; A alma de Kikyou (Parte 1)',8,2052,112),
+(null, 'A verdade sobre a origem de Naraku; A alma de Kikyou (Parte 2)',8,2052,112),
+(null, 'A voz de Kagome e o beijo de Kikyou',8,2052,112),
+(null, 'Sango, a exterminadora de youkais',8,2052,112),
+(null, 'Derrotando o plano de Naraku',8,2052,112),
+(null, 'O segredo da Jóia de Quatro Almas é finalmente revelado',8,2052,112),
+(null, 'O Lago da Escuridão controlado pelo Deus da Água',8,2052,112),
+(null,'Miroku cai numa armadilha',9,2052,112),
+(null,'A angústia de Sango e a vida de Kohaku',9,2052,112),
+(null,'A Tessaiga roubada e o confronto no castelo de Naraku',9,2052,112),
+(null,'Jinenji, o youkai bondoso e solitário',9,2052,112),
+(null,'Kikyou e Inuyasha atraídos pelo Miasma',9,2052,112),
+(null,'Kikyou é raptada',9,2052,112),
+(null,'Tesaiga e Tenseiga',9,2052,112),
+(null,'A célebre espada escolhe o seu verdadeiro mestre',9,2052,112),
+(null,'O rapto de Kagome; Kouga, o youkai lobo superveloz',9,2052,112),
+(null,'O youkai que se apaixonou por Kagome',9,2052,112),
+(null,'Separados, os dois se dão conta dos seus sentimentos',9,2052,112),
+(null,'Um plano ardiloso; A luta de vida ou morte',9,2052,112),
+(null,'A armadilha de Kagura, a mestra dos ventos',9,2052,112),
+(null,'A dança de Kagura e o espelho de Kanna',9,2052,112),
+(null,'A Ferida do Vento refletida',9,2052,112),
+(null,'A Tesaiga é finalmente quebrada',9,2052,112),
+(null,'A espada malígna de Kaijinbou',9,2052,112),
+(null,'Sesshoumaru saca Toukijin',9,2052,112),
+(null,'Juuroumaru e Kageroumaru',9,2052,112),
+(null,'O coração de Onigumo ainda vive em Naraku',9,2052,112),
+(null,'Quero voltar ao lugar onde nos conhecemos',9,2052,112),
+(null,'A memória perdida de Kohaku',9,2052,112),
+(null,'O rosto que não sai do meu coração',9,2052,112),
+(null,'O coração consumido de Inuyasha',9,2052,112),
+(null,'A verdadeira natureza do youkai não pode ser reprimida',9,2052,112),
+(null,'O inimigo mortal do meu pai: Ryoukossei',9,2052,112),
+(null,'Onda Explosiva: o ataque supremo da Tessaiga',9,2052,112),
+(null,'A flor de pedra e o primeiro amor de Shippou',10,2052,112),
+(null,'A armadilha da bela princesa na névoa',10,2052,112),
+(null,'Tudo o que aconteceu na noite em Tougenkyou (Parte 1)',10,2052,112),
+(null,'Tudo o que aconteceu na noite em Tougenkyou (Parte 2)',10,2052,112),
+(null,'As belas irmãs aprendizes',10,2052,112),
+(null,'A sacerdotisa das sombras; Uma maldição de 50 anos',10,2052,112),
+(null,'Kikyou aparece!; O uso do Shikigami',10,2052,112),
+(null,'A maldição cruel de Tsubaki',10,2052,112),
+(null,'A sacerdotisa azul e a sacerdotisa vermelha que bloqueiam o caminho',10,2052,112),
+(null,'Tahou, o youkai gigante da torre!',10,2052,112),
+(null,'Adeus aos dias de juventude',10,2052,112),
+(null,'A barreira de Naraku; A decisão de Kagura',10,2052,112),
+(null,'O devastador vento da traição',10,2052,112),
+(null,'O grande desafio de Shippou',10,2052,112),
+(null,'O terrível homem sem rosto',10,2052,112),
+(null,'A memória recuperada de Onigumo',10,2052,112),
+(null,'A batalha dos três até à morte',10,2052,112),
+(null,'O estranho treino de Toutousai',10,2052,112),
+(null,'A mãe e os sentimentos de Shiori',10,2052,112),
+(null,'A Tessaiga vermelha que quebra a barreira',10,2052,112),
+(null,'A conspiração dos quatro youkais gatos de fogo',10,2052,112),
+(null,'Os alvos são: Sesshoumaru e Inuyasha',10,2052,112),
+(null,'Os gatos de fogo e a espada de dois caninos',10,2052,112),
+(null,'Sango, só você',10,2052,112),
+(null,'O plano de Jaken para roubar a Tessaiga',10,2052,112),
+(null,'Sesshoumaru e o rapto de Rin',10,2052,112),
+(null,'Naraku despedaçado; E seu paradeiro?',10,2052,112),
+(null,'A ligação entre o Presente e a Época das Guerras',10,2052,112),
+(null,'A youkai loba e a grande promessa',11,2052,112),
+(null,'O proposta de casamento super veloz',11,2052,112),
+(null,'O aumento da energia maligna; A cabeça do youkai',11,2052,112),
+(null,'O segredo da princesa',11,2052,112),
+(null,'A viagem solitária de Kikyou',11,2052,112),
+(null,'Os três youkais do Deus Macaco',11,2052,112),
+(null,'O confronto entre Inuyasha e Houjo',11,2052,112),
+(null,'A corajosa declaração de Souta',11,2052,112),
+(null,'O espiritualista misterioso e a Kirara negra',11,2052,112),
+(null,'A ambição dos ressuscitados',11,2052,112),
+(null,'O enigma do monge devasso',11,2052,112),
+(null,'Os criadores da Jóia de Quatro Almas (Parte 1)',11,2052,112),
+(null,'Os criadores da Jóia de Quatro Almas (Parte 2)',11,2052,112),
+(null,'Jaken fica doente',11,2052,112),
+(null,'Kirara não volta',11,2052,112),
+(null,'Kikyou e Kagome sozinhas numa caverna',11,2052,112),
+(null,'Kouga e Sesshoumaru! Um perigoso confronto!',11,2052,112),
+(null,'Pesadelo Real! A batalha na Floresta do Desespero',11,2052,112),
+(null,'Sete anos depois: As neves eternas',11,2052,112),
+(null,'O líder dos youkais lobos é atacado por um zombi',11,2052,112),
+(null,'O exército dos sete ressuscitados',11,2052,112),
+(null,'Mukotsu, o envenenador',11,2052,112),
+(null,'O terrível guerreiro de aço, Guinkoutsu',11,2052,112),
+(null,'Sepultados sob as chamas',11,2052,112),
+(null,'As primeiras lágrimas de Inuyasha',11,2052,112),
+(null,'O segredo do explendor puro',11,2052,112),
+(null,'O monte sagrado Hakurei',11,2052,112),
+(null,'Surge o líder dos Shichinintai: Bankotsu',11,2052,112),
+(null,'Acerto de contas: Banryuu contra Ferida do Vento',12,2052,112),
+(null,'A barreira na Ilha Hijiri',12,2052,112),
+(null,'O Dokko sagrado e o segredo da múmia',12,2052,112),
+(null,'A batalha solitária de Kouga',12,2052,112),
+(null,'Engolido pela luz negra',12,2052,112),
+(null,'A nova aparência de Naraku',12,2052,112),
+(null,'O rio de fogo',12,2052,112),
+(null,'No coração do Monte Hakurei',12,2052,112),
+(null,'Um Santo divino e malicioso',12,2052,112),
+(null,'Adeus!; Uma marcha fúnebre para Jakotsu',12,2052,112),
+(null,'A batalha final!; O último e mais forte dos Shichinintai',12,2052,112),
+(null,'Poderosa Banryuu!; O combate mortal dentro do Monte Hakurei',12,2052,112),
+(null,'Naraku regenerado além da escuridão',12,2052,112),
+(null,'Adeus, minha querida Kikyou',12,2052,112),
+(null,'A escuridão no coração de Kagome',12,2052,112),
+(null,'Transformando a dor em coragem',12,2052,112),
+(null,'Os terríveis youkais secos',12,2052,112),
+(null,'Os youkais invadem o festival cultural',12,2052,112),
+(null,'Cho Kyukai e as noivas roubadas',12,2052,112),
+(null,'O rugido e a ferida no coração de Shippou',12,2052,112),
+(null,'Os pergaminhos de Kannon',12,2052,112),
+(null,'A mais perigosa confissão de Miroku',12,2052,112),
+(null,'A mulher que amou Sesshoumaru (Parte 1)',12,2052,112),
+(null,'A mulher que amou Sesshoumaru (Parte 2)',12,2052,112),
+(null,'O último banquete do mestre de Miroku',12,2052,112),
+(null,'Um youkai invisível aparece!',12,2052,112),
+(null,'O nome da ancestral é Kagome',12,2052,112),
+(null,'A Sobrevivência numa terra infestada de youkais',12,2052,112),
+(null,'O grande duelo na cascata Shohun',13,2052,112),
+(null,'Sentimentos eternos',13,2052,112),
+(null,'A libertação do monstruoso cavalo youkai Entei',13,2052,112),
+(null,'A fuga de Entei',13,2052,112),
+(null,'A viagem em busca do pai',13,2052,112),
+(null,'Housenki e o último fragmento',13,2052,112),
+(null,'Os estranhos guardiões do portal',13,2052,112),
+(null,'Abi, a incontrolável dominadora de aves',13,2052,112),
+(null,'Uma fatídica canção de amor antes de nos encontrarmos (Parte 1)',13,2052,112),
+(null,'Uma fatídica canção de amor antes de nos encontrarmos (Parte 2)',13,2052,112),
+(null,'A flecha que causa problemas',13,2052,112),
+(null,'A luz que guia à um santo',13,2052,112),
+(null,'A intuição de Kagome',13,2052,112),
+(null,'Proteger e roubar',13,2052,112),
+(null,'Uma Reunião Cruel',13,2052,112),
+(null,'A youkai que conecta com o outro Mundo',13,2052,112),
+(null,'O youkai que protege o fragmento',13,2052,112),
+(null,'Sesshoumaru enfrenta Inuyasha',13,2052,112),
+(null,'Aniquilando Naraku',13,2052,112),
+(null,'Uma Correria de Ratos youkai',13,2052,112),
+(null,'A Decisão de Kohaku',13,2052,112),
+(null,'Um Jovem violento que atrai a Felicidade',13,2052,112),
+(null,'O erro no Passado do monge Miroku',13,2052,112),
+(null,'Para sempre junto de Sesshoumsama',13,2052,112),
+(null,'Kohaku, Sango, Kirara e o Jardim Secreto da Flor',13,2052,112),
+(null,'O maior dos inimigo; Pupa parasita de Shippou',13,2052,112),
+(null,'A Grande Pista para Derrotar Naraku',13,2052,112),
+(null,'O compromisso do casal para o uso da Jóia (parte 1)',13,2052,112),
+(null,'O compromisso do casal para o uso da Jóia (parte 2)',13,2052,112);
+
+insert into capitulo
+(null,'InuYasha capitulo1',8,2051,2051,null,null,105),
+(null,'InuYasha capitulo2',8,2051,2051,null,null,105),
+(null,'InuYasha capitulo3',8,2051,2051,null,null,105),
+(null,'InuYasha capitulo4',8,2051,2051,null,null,105),
+(null,'InuYasha capitulo5',8,2051,2051,null,null,105),
+(null,'InuYasha capitulo6',8,2051,2051,null,null,105),
+(null,'InuYasha capitulo7',8,2051,2051,null,null,105),
+(null,'InuYasha capitulo8',8,2051,2051,null,null,105),
+(null,'InuYasha capitulo9',8,2051,2051,null,null,105),
+(null,'InuYasha capitulo93',8,2051,2051,null,null,105),
+(null,'InuYasha capitulo94',9,2051,2051,null,null,105),
+(null,'InuYasha capitulo95',9,2051,2051,null,null,105),
+(null,'InuYasha capitulo96',9,2051,2051,null,null,105),
+(null,'InuYasha capitulo97',9,2051,2051,null,null,105),
+(null,'InuYasha capitulo98',9,2051,2051,null,null,105),
+(null,'InuYasha capitulo99',9,2051,2051,null,null,105),
+(null,'InuYasha capitulo186',9,2051,2051,null,null,105),
+(null,'InuYasha capitulo187',9,2051,2051,null,null,105),
+(null,'InuYasha capitulo188',9,2051,2051,null,null,105),
+(null,'InuYasha capitulo189',9,2051,2051,null,null,105),
+(null,'InuYasha capitulo190',9,2051,2051,null,null,105),
+(null,'InuYasha capitulo191',10,2051,2051,null,null,105),
+(null,'InuYasha capitulo192',10,2051,2051,null,null,105),
+(null,'InuYasha capitulo193',10,2051,2051,null,null,105),
+(null,'InuYasha capitulo194',10,2051,2051,null,null,105),
+(null,'InuYasha capitulo195',10,2051,2051,null,null,105),
+(null,'InuYasha capitulo196',10,2051,2051,null,null,105),
+(null,'InuYasha capitulo197',10,2051,2051,null,null,105),
+(null,'InuYasha capitulo198',10,2051,2051,null,null,105),
+(null,'InuYasha capitulo199',10,2051,2051,null,null,105),
+(null,'InuYasha capitulo279',10,2051,2051,null,null,105),
+(null,'InuYasha capitulo280',11,2051,2051,null,null,105),
+(null,'InuYasha capitulo281',11,2051,2051,null,null,105),
+(null,'InuYasha capitulo282',11,2051,2051,null,null,105),
+(null,'InuYasha capitulo283',11,2051,2051,null,null,105),
+(null,'InuYasha capitulo284',11,2051,2051,null,null,105),
+(null,'InuYasha capitulo285',11,2051,2051,null,null,105),
+(null,'InuYasha capitulo286',11,2051,2051,null,null,105),
+(null,'InuYasha capitulo287',11,2051,2051,null,null,105),
+(null,'InuYasha capitulo288',11,2051,2051,null,null,105),
+(null,'InuYasha capitulo289',11,2051,2051,null,null,105),
+(null,'InuYasha capitulo372',11,2051,2051,null,null,105),
+(null,'InuYasha capitulo373',12,2051,2051,null,null,105),
+(null,'InuYasha capitulo374',12,2051,2051,null,null,105),
+(null,'InuYasha capitulo375',12,2051,2051,null,null,105),
+(null,'InuYasha capitulo376',12,2051,2051,null,null,105),
+(null,'InuYasha capitulo377',12,2051,2051,null,null,105),
+(null,'InuYasha capitulo378',12,2051,2051,null,null,105),
+(null,'InuYasha capitulo379',12,2051,2051,null,null,105),
+(null,'InuYasha capitulo380',12,2051,2051,null,null,105),
+(null,'InuYasha capitulo465',12,2051,2051,null,null,105),
+(null,'InuYasha capitulo466',13,2051,2051,null,null,105),
+(null,'InuYasha capitulo467',13,2051,2051,null,null,105),
+(null,'InuYasha capitulo468',13,2051,2051,null,null,105),
+(null,'InuYasha capitulo469',13,2051,2051,null,null,105),
+(null,'InuYasha capitulo470',13,2051,2051,null,null,105),
+(null,'InuYasha capitulo471',13,2051,2051,null,null,105),
+(null,'InuYasha capitulo472',13,2051,2051,null,null,105),
+(null,'InuYasha capitulo558',13,2051,2051,null,null,105);
