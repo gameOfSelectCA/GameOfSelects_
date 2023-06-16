f3b860224a0a44fd542aa23bc9917aea4cdf1421
GabrielYukioMC <gabriel.coca@sptech.school>
2023-06-16 14:04:05 -0300

    selects 25%

--- selects.sql
+++ selects.sql
@@ -1,6 +1,6 @@
--- *1  select do nome do autor buscando apenas os vivos e que a data de nasc começe com 1
+-- *1  select do nome do autor buscando apenas os vivos e que a data de nasc começe com 1do mais velho pro mais novo
 
-SELECT nome FROM autor where statusvida = 'vivo' and dtNasc like '1%' order by nome;
+SELECT nome FROM autor where statusvida = 'vivo' and dtNasc like '1%' order by dtNasc;
 
 -- *2 select do idMangaka e o nome do mangaka buscando as datas de nasc indefinidas 
 
@@ -42,18 +42,34 @@ select idAutor, nome, dtNasc from autor  where nome like '%O%' order by idAutor 
 select idAutor, nome, dtNasc from autor  where nome like '%O%' order by idAutor desc limit 1;
 
 
--- *11 fazer um select que pegue todos os itens da tabela manga
-select * from manga;
+-- *11 fazer um select que pegue todos os itens da tabela manga em order decrescente pelo id
+select * from manga order by idManga desc;
 
 
--- *12 fazer um select que pegue todos os itens da tabela manga
-select * from manga;
+-- *12 fazer um select que pegue todos os dados da tabela episodio onde o nome possua a letra "A" em qualquer posição
+select * from episodio where  nome like '%A%';
 
--- *13 fazer um select que pegue todos os itens da tabela manga
-select * from manga;
+-- *13 fazer um select que pegue os 2 primeiros dados da tabela autor
+select * from autor limit 2;
 
--- *14 fazer um select que pegue todos os itens da tabela manga
-select * from manga;
+-- *14 fazer um select que deixe os dados de maneira decrecente ordenandos pelo id e pegue os 2 ultimos dados da tabela autor
+select * from autor  order by idAutor desc limit 2 ;
 
--- *15 fazer um select que pegue todos os itens da tabela saga cujo o nome seja East Blue
+-- *15 fazer um select que pegue todos os itens da tabela saga cujo o nome seja "East Blue"
 select * from saga where nome='East Blue';
+
+
+-- ^ 01 me informe o nome dos animes, o nome de suas produtoras e as datas de criação dessas produtoras nessa order  apenas dos animes que tenham a palavra "ONE" em seus nomes 
+
+select anime.nome , produtora.nome , produtora.dtCriacao from anime join produtora on fkProdutora = idProdutora where anime.nome like '%One%';
+
+
+-- ^ 02 me informe os id's das sagas, os nomes das sagas e os nomes das produtoras  apenas das sagas que possuem o id maior do que 7 e menor do que 11 de forma que os dados fiquem em uma order crescente ordenada pelo id
+
+select idSaga, saga.nome , idProdutora,  produtora.nome from saga join produtora on fkProdutoraSaga = idProdutora where idSaga > 7 and idsaga <11 and produtora.nome like '%T%' order by idsaga;
+
+-- ^ 03 
+
+select * from anime join obra on fkObraAnime=idObra ;
+
+select * from episodio;
